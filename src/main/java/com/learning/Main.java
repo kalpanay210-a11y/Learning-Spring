10a41edaa50032edf096387adc538d5262dac9b3
kalpanay210-a11y <kalpanay210@gmail.com>
2026-07-05 11:13:40 +0530

    update

---
+++ src/main/java/com/learning/Main.java
@@ -0,0 +1,21 @@
+package com.learning;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.learning.model.Employee;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		
+		ApplicationContext ioc = new ClassPathXmlApplicationContext("application-context.xml");
+		
+		Employee bean1 = ioc.getBean("emp1", Employee.class);
+		System.out.println(bean1);
+		
+		Employee bean2 = ioc.getBean("emp1", Employee.class);
+		System.out.println(bean2);
+	}
+
+}
