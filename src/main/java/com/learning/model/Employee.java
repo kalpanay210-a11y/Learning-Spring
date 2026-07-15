cf815fb6515ef5195560fdca4158ca2e8798ef4f
kalpanay210-a11y <kalpanay210@gmail.com>
2026-07-15 23:01:30 +0530

    update

--- src/main/java/com/learning/model/Employee.java
+++ src/main/java/com/learning/model/Employee.java
@@ -1,8 +1,15 @@
 package com.learning.model;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+
 @Data
+@AllArgsConstructor
 public class Employee {
 	
 	private int id;
@@ -11,66 +18,14 @@ public class Employee {
 	
 	private Address address;
 	
+	private List<Integer> list;
+	
+	private Set<Integer> set;
+	
+	private Map<Integer, String> map;
+	
 	public Employee() {
 		System.out.println("Employee.Employee()");
 	}
 	
-	public Employee(int id, String name, String gender, int salary, Address address) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.gender = gender;
-		this.salary = salary;
-		this.address = address;
-		System.out.println("Employee.Employee(5)");
-	}
-	
-	public int getId() {
-		return id;
-	}
-	
-	public void setId(int id) {
-		this.id = id;
-		System.out.println("Employee.setId()");
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name= name;
-		System.out.println("Employee.setName()");
-	}
-	
-	public String getGender() {
-		return gender;
-	}
-	
-	public void setGender(String gender) {
-		this.gender = gender;
-		System.out.println("Employee.setGender()");
-	}
-	
-	public int getSalary() {
-		return salary;
-	}
-	
-	public void setSalary(int salary) {
-		this.salary = salary;
-		System.out.println("Employee.setSalary()");
-	}
-	 public Address getAddress() {
-		 return address;
-	 }
-	
-	 public void setAddress(Address address) {
-		 this.address = address;
-	 }
-	 
-	 @Override 
-	 
-	 public String toString() {
-		 return "Employee[ name= "+ name +", gender= "+ gender +", salary= "+ gender+", address= "+ address +"]";
-	 }
 }
